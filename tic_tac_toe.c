8535a20cf53644dabe86cd7222c93920ebd04299
Michael <michaelgss009@gmail.com>
2026-03-11 09:01:04 +0100

    ttt game

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 
 #define GRID_SIZE 3
-char grid[GRID_SIZE][GRID_SIZE] = {
+char* grid[GRID_SIZE][GRID_SIZE] = {
     {' ', ' ', ' '},
     {' ', ' ', ' '},
     {' ', ' ', ' '}
 };
+
+void set_symbol(int row, int col, char* symbol) {
+    if (row <= 2 && col <=2) {
+        grid[row][col] = symbol;
+    };
+}
+
+void print_grid() {
+    for (int i=0; i<GRID_SIZE; i++) {
+        for (int j = 0;j<GRID_SIZE; j++) {
+            printf("%c", grid[i][j]);
+        }
+        printf("\n");
+    };
+
+}
+
+int main() {
+
+    print_grid();
+    set_symbol(0, 0, "X");
+    print_grid();
+
+    return 0;
+}
